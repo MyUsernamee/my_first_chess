1a1cdf9f2ac045feaf4ecf13a1b52afeb6b90672
Delano Leslie <delanoleslieprof@gmail.com>
2024-06-02 00:07:20 -0700

    First 5 moves

--- generate.py
+++ generate.py
@@ -1,11 +1,6 @@
 import chess
 import tqdm
-
 board = chess.Board()
-
-print(board)
-
-print(board.legal_moves)
 
 def add_move(output: str, board: chess.Board, move, tabs=0, depth=4):
 
@@ -18,12 +13,12 @@ def add_move(output: str, board: chess.Board, move, tabs=0, depth=4):
     board_string = str(new_board).replace("\n", "\\n")
 
     # Add the board to the output
-    output += "\t"* tabs + "if move == \"" + move.uci() + "\":\n"
-    output += "\t"* tabs + "\t" + "print(\"" + board_string + "\")\n"
+    print("\t"* tabs + "if move == \"" + move.uci() + "\":")
+    print("\t"* tabs + "\t" + "print(\"" + board_string + "\")")
 
     if board.is_checkmate():
-        output += "\t"* tabs + "\t" + "print(\"Checkmate\")\n"
-        output += "\t"* tabs + "\t" + "exit()\n"
+        print("\t"* tabs + "\t" + "print(\"Checkmate\")")
+        print("\t"* tabs + "\t" + "exit()")
         return output
 
     output = new_turn(output, new_board, tabs+1)
@@ -31,8 +26,8 @@ def add_move(output: str, board: chess.Board, move, tabs=0, depth=4):
     for move in new_board.legal_moves:
 
         if depth == 1:
-            output += "\t"* tabs + "\t" + "print(\"Sorry, I can't go any deeper\")\n"
-            output += "\t"* tabs + "\t" + "exit()\n"
+            print("\t"* tabs + "\t" + "print(\"Sorry, I can't go any deeper\")")
+            print("\t"* tabs + "\t" + "exit()")
             break
 
         
@@ -44,9 +39,9 @@ def new_turn(output: str, board: chess.Board, tabs=0):
 
     # Add the board to the output
     tabs_string = "\t"* tabs
-    output += tabs_string + "print(\"" + str(board).replace("\n", "\\n") + "\")\n"
-    output += tabs_string + "print(\"" + ("White's " if board.turn == chess.WHITE else "Black's ") + " turn\")\n"
-    output += tabs_string + "move = input(\"Enter your move: \")\n"
+    print(tabs_string + "print(\"" + str(board).replace("\n", "\\n") + "\")")
+    print(tabs_string + "print(\"" + ("White's " if board.turn == chess.WHITE else "Black's ") + " turn\")")
+    print(tabs_string + "move = input(\"Enter your move: \")")
 
     return output
 
@@ -56,7 +51,7 @@ for position in range(0, 1):
     output = new_turn(output, board)
 
     for move in tqdm.tqdm(board.legal_moves):
-        output = add_move(output, board, move, 0, 4)
+        output = add_move(output, board, move, 0, 5)
 
 #Save the output to a python file
 with open("game.py", "w") as file:
